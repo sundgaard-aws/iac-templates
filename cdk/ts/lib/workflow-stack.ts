4968159ab4a20a894bfb8ffa15f898c71af1e69b
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2020-10-05 20:33:10 +0200

    New TS stack

--- cdk/ts/lib/workflow-stack.ts
+++ cdk/ts/lib/workflow-stack.ts
@@ -1,10 +1,11 @@
-import * as cdk from '@aws-cdk/core';
+import * as Core from '@aws-cdk/core';
 import EC2 = require('@aws-cdk/aws-ec2');
 import S3 = require('@aws-cdk/aws-s3');
 import SQS = require('@aws-cdk/aws-sqs');
 import Lambda = require('@aws-cdk/aws-lambda');
 import StepFunctions = require('@aws-cdk/aws-stepfunctions');
 import StepFunctionsTasks = require('@aws-cdk/aws-stepfunctions-tasks');
+import { IVpc } from '@aws-cdk/aws-ec2';
 
 const PREFIX = "iac-demo-";
 const NAME = "Name";
@@ -15,15 +16,15 @@ const NAME = "Name";
 
 namespace Dotnet
 {
-    public class WorkflowStack : Stack
-    {
-        internal WorkflowStack(Construct scope, string id, string vpcRef, Vpc vpc, IStackProps props = null) : base(scope, id, props)
-        {
-            createSQSQueue(vpcRef);
-            createStepFunctionStates(vpcRef, vpc);
+    export class WorkflowStack extends Core.Stack {
+        constructor(scope: Core.Construct, id: string, vpcRef: string, vpc: IVpc, props?: Core.StackProps) {
+            super(scope, id, props);
+
+            this.createSQSQueue(vpcRef);
+            this.createStepFunctionStates(vpcRef, vpc);
         }
 
-        private void createStepFunctionStates(string vpcRef, Vpc vpc)
+        private createStepFunctionStates(vpcRef: string, vpc: IVpc)
         {
             /*var stepFunctions = new Amazon.CDK.AWS.StepFunctions.CfnStateMachine(this, Program.PREFIX+"stf", new CfnStateMachineProps {
                 StateMachineType = StateMachineType.STANDARD.ToString(), StateMachineName = Program.PREFIX+"stf"
@@ -32,15 +33,15 @@ namespace Dotnet
                 VpcId = vpcRef
             });*/
 
-            var codeBucket = new Bucket(this, Program.PREFIX+"lambda-code-bucket", new BucketProps {
-                BucketName = Program.PREFIX+"lambda-code-bucket"
+            var codeBucket = new S3.Bucket(this, PREFIX+"lambda-code-bucket", {
+                bucketName = PREFIX+"lambda-code-bucket"
             });            
 
-            var runtime = Runtime.DOTNET_CORE_3_1;
-            var submitFunctionCodeFromS3 = new S3Code(codeBucket, "submit-api-code.zip");
-            var submitFunctionCodeFromLocalZip = Code.FromAsset("submit-api.zip");
-            var submitLambda = new Function(this, "SubmitLambda", new FunctionProps { 
-                FunctionName = Program.PREFIX + "submit-api-lfn", Vpc = vpc, Code = submitFunctionCodeFromLocalZip, Handler = "IAC.Demo.FunctionHandler", Runtime = runtime
+            var runtime = Lambda.Runtime.DOTNET_CORE_3_1;
+            var submitFunctionCodeFromS3 = new Lambda.S3Code(codeBucket, "submit-api-code.zip");
+            var submitFunctionCodeFromLocalZip = Lambda.Code.fromAsset("submit-api.zip");
+            var submitLambda = new Function(this, "SubmitLambda", { 
+                FunctionName: PREFIX + "submit-api-lfn", Vpc: vpc, Code: submitFunctionCodeFromLocalZip, Handler: "IAC.Demo.FunctionHandler", Runtime: runtime
             });
 
             /*var test = new CfnFunction(this, "id", new CfnFunctionProps {
@@ -48,56 +49,57 @@ namespace Dotnet
             });*/
             
 
-            var statusFunctionCodeFromS3 = new S3Code(codeBucket, "status-api-code.zip");
-            var statusFunctionCodeFromLocalZip = Code.FromAsset("submit-api.zip");
-            var getStatusLambda = new Function(this, "CheckLambda", new FunctionProps { 
-                FunctionName = Program.PREFIX + "check-api-lfn", Vpc = vpc, Code = statusFunctionCodeFromLocalZip, Handler = "IAC.Demo.FunctionHandler", Runtime = runtime
+            var statusFunctionCodeFromS3 = new Lambda.S3Code(codeBucket, "status-api-code.zip");
+            var statusFunctionCodeFromLocalZip = Lambda.Code.fromAsset("submit-api.zip");
+            var getStatusLambda = new Function(this, "CheckLambda", { 
+                FunctionName: PREFIX + "check-api-lfn", Vpc: vpc, Code: statusFunctionCodeFromLocalZip, Handler: "IAC.Demo.FunctionHandler", Runtime: runtime
             });
 
-            var submitJob = new LambdaInvoke(this, "Submit Job", new LambdaInvokeProps {
-                LambdaFunction = submitLambda,
+            var submitJob = new StepFunctionsTasks.LambdaInvoke(this, "Submit Job", {
+                lambdaFunction: submitLambda,
                 // Lambda's result is in the attribute `Payload`
                 OutputPath = "$.Payload"
             });
 
-            var waitX = new Wait(this, "Wait X Seconds", new WaitProps {
-                Time = WaitTime.SecondsPath("$.waitSeconds")
+            var waitX = new StepFunctions.Wait(this, "Wait X Seconds", {
+                time: WaitTime.SecondsPath("$.waitSeconds")
             });
 
-            var getStatus = new LambdaInvoke(this, "Get Job Status", new LambdaInvokeProps {
-                LambdaFunction = getStatusLambda,
+            var getStatus = new StepFunctionsTasks.LambdaInvoke(this, "Get Job Status", {
+                lambdaFunction: getStatusLambda,
                 // Pass just the field named "guid" into the Lambda, put the
                 // Lambda's result in a field called "status" in the response
                 InputPath = "$.guid",
                 OutputPath = "$.Payload"
             });
 
-            var jobFailed = new Fail(this, "Job Failed", new FailProps {
+            var jobFailed = new Fail(this, "Job Failed", {
                 Cause = "AWS Batch Job Failed",
                 Error = "DescribeJob returned FAILED"
             });
 
-            var finalStatus = new LambdaInvoke(this, "Get Final Job Status", new LambdaInvokeProps {
-                LambdaFunction = getStatusLambda,
+            var finalStatus = new StepFunctionsTasks.LambdaInvoke(this, "Get Final Job Status", {                
+                lambdaFunction: getStatusLambda,
                 // Use "guid" field as input
                 InputPath = "$.guid",
                 OutputPath = "$.Payload"
             });
 
-            var definition = submitJob.Next(waitX).Next(getStatus).Next(new Choice(this, "Job Complete?").When(Condition.StringEquals("$.status", "FAILED"), jobFailed).When(Condition.StringEquals("$.status", "SUCCEEDED"), finalStatus).Otherwise(waitX));
+            var definition = submitJob.Next(waitX).Next(getStatus).Next(new StepFunctions.Choice(this, "Job Complete?").when(StepFunctions.Condition.stringEquals("$.status", "FAILED"), jobFailed)
+            .when(StepFunctions.Condition.stringEquals("$.status", "SUCCEEDED"), finalStatus).otherwise(waitX));
 
-            new StateMachine(this, "StateMachine", new StateMachineProps {
-                Definition = definition,
-                Timeout = Duration.Minutes(5)
+            new StepFunctions.StateMachine(this, "StateMachine", {
+                definition: definition,
+                timeout: Core.Duration.minutes(5)
             });
         }
 
-        private void createSQSQueue(string vpcRef)
+        private createSQSQueue(vpcRef: string)
         {
-            var queue = new Amazon.CDK.AWS.SQS.CfnQueue(this, Program.PREFIX+"sqs", new CfnQueueProps {
-                QueueName = Program.PREFIX+"sqs", VisibilityTimeout = 4, MessageRetentionPeriod = 360000
+            var queue = new SQS.CfnQueue(this, PREFIX+"sqs", {
+                queueName: PREFIX+"sqs", visibilityTimeout = 4, messageRetentionPeriod = 360000
             });
-            queue.Tags.SetTag("Name", Program.PREFIX+"sqs");
+            queue.tags.setTag("Name", PREFIX+"sqs");
         }
     }
 }
