bd84d05165a54104b41809079830f24ee5af7112
Michael Sundgaard <sundgaar@amazon.com>
2020-10-29 09:41:27 +0000

    Added ALB

--- cdk/ts/lib/classic-web-stack-l2.ts
+++ cdk/ts/lib/classic-web-stack-l2.ts
@@ -8,55 +8,22 @@ import Lambda = require('@aws-cdk/aws-lambda');
 import StepFunctions = require('@aws-cdk/aws-stepfunctions');
 import StepFunctionsTasks = require('@aws-cdk/aws-stepfunctions-tasks');
 import * as ASC from '@aws-cdk/aws-autoscaling';
-import * as ELB from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as ELBv2 from '@aws-cdk/aws-elasticloadbalancingv2';
 import { InstanceType, IVpc } from '@aws-cdk/aws-ec2';
 import { MetaData } from './meta-data';
 import { CfnListener, CfnLoadBalancer, CfnTargetGroup } from '@aws-cdk/aws-elasticloadbalancingv2';
 
 export class ClassicWebStackL2 extends Core.Stack {
-    private targetGroup: ELB.CfnTargetGroup;
+    private targetGroup: ELBv2.CfnTargetGroup;
     
     constructor(scope: Core.Construct, id: string, metaData: MetaData, props?: Core.StackProps) {
         super(scope, id, props);
         console.log("region="+props?.env?.region);
         
-        this.createLoadBalancer(metaData);
-        this.createAutoScalingGroup(metaData);
+        var lbSecurityGroup = this.buildLoadBalancerSecurityGroup(metaData);
+        var autoScalingGroup = this.createAutoScalingGroup(metaData, lbSecurityGroup);
+        this.createLoadBalancer(metaData, lbSecurityGroup, autoScalingGroup);
         //this.createAutoScalingGroupL2(vpcRef, vpc);
-    }
-    
-    private createAutoScalingGroup(metaData: MetaData) {
-        const amznLinux = EC2.MachineImage.latestAmazonLinux({
-            generation: EC2.AmazonLinuxGeneration.AMAZON_LINUX_2
-            /*edition: EC2.AmazonLinuxEdition.MINIMAL,
-            virtualization: EC2.AmazonLinuxVirt.HVM,
-            storage: EC2.AmazonLinuxStorage.GENERAL_PURPOSE,
-            cpuType: EC2.AmazonLinuxCpuType.X86_64*/
-        });
-        
-        var userData = EC2.UserData.custom(this.buildJavaEnabledServer());
-        var loadBalancerSecurityGroup = this.buildLoadBalancerSecurityGroup(metaData);
-        // AutoScaling groups can't be updated if the name persists, so we wan't a dynamic name but with a consistent prefix
-        var asgName = metaData.PREFIX+"web-asg-"+UUID.v4();
-
-        var asg = new ASC.AutoScalingGroup(this, metaData.PREFIX+"web-asg", {
-            machineImage: amznLinux, 
-            vpc: metaData.VPC,
-            updateType: ASC.UpdateType.REPLACING_UPDATE,
-            securityGroup: this.buildWebSecurityGroup(metaData,loadBalancerSecurityGroup),
-            vpcSubnets: { subnets: metaData.VPC.privateSubnets },
-            role: this.buildWebRole(metaData),
-            instanceType: EC2.InstanceType.of(EC2.InstanceClass.BURSTABLE3, EC2.InstanceSize.MICRO),
-            associatePublicIpAddress: false,
-            desiredCapacity: 2,
-            maxCapacity: 4,
-            minCapacity:0,
-            autoScalingGroupName: asgName,
-            userData: userData
-        });
-
-        Core.Tags.of(asg).add(metaData.NAME, metaData.PREFIX+"web-asg");
-        Core.Tags.of(asg).add(metaData.NAME, metaData.PREFIX+"web-ec2", { includeResourceTypes: [EC2.CfnInstance.CFN_RESOURCE_TYPE_NAME] });
     }
     
     private buildLoadBalancerSecurityGroup(metaData:MetaData): EC2.ISecurityGroup {
@@ -71,6 +38,56 @@ export class ClassicWebStackL2 extends Core.Stack {
         Core.Tags.of(securityGroup).add(metaData.NAME, metaData.PREFIX+"web-lb-sg");
         return securityGroup;
     }    
+    
+    private createLoadBalancer(metaData: MetaData, lbSecurityGroup: EC2.ISecurityGroup, autoScalingGroup: ASC.IAutoScalingGroup) {
+        var alb = new ELBv2.ApplicationLoadBalancer(this, metaData.PREFIX+"web-alb", {
+           internetFacing: true,
+           loadBalancerName: metaData.PREFIX+"web-alb",
+           vpc: metaData.VPC,
+           vpcSubnets: { subnetType: EC2.SubnetType.PUBLIC },
+           securityGroup: lbSecurityGroup
+        });
+        Core.Tags.of(alb).add(metaData.NAME, metaData.PREFIX+"web-alb");
+        
+        const listener = alb.addListener('Listener', {
+          port: 80,
+          open: true, // 'open: true' is the default, you can leave it out if you want. Set it to 'false' and use `listener.connections` if you want to be selective about who can access the load balancer.
+        });        
+    }
+    
+    private createAutoScalingGroup(metaData: MetaData, lbSecurityGroup: EC2.ISecurityGroup): ASC.IAutoScalingGroup {
+        const amznLinux = EC2.MachineImage.latestAmazonLinux({
+            generation: EC2.AmazonLinuxGeneration.AMAZON_LINUX_2
+            /*edition: EC2.AmazonLinuxEdition.MINIMAL,
+            virtualization: EC2.AmazonLinuxVirt.HVM,
+            storage: EC2.AmazonLinuxStorage.GENERAL_PURPOSE,
+            cpuType: EC2.AmazonLinuxCpuType.X86_64*/
+        });
+        
+        var userData = EC2.UserData.custom(this.buildJavaEnabledServer());
+        // AutoScaling groups can't be updated if the name persists, so we wan't a dynamic name but with a consistent prefix
+        var asgName = metaData.PREFIX+"web-asg-"+UUID.v4();
+
+        var asg = new ASC.AutoScalingGroup(this, metaData.PREFIX+"web-asg", {
+            machineImage: amznLinux, 
+            vpc: metaData.VPC,
+            updateType: ASC.UpdateType.REPLACING_UPDATE,
+            securityGroup: this.buildWebSecurityGroup(metaData, lbSecurityGroup),
+            vpcSubnets: { subnets: metaData.VPC.privateSubnets },
+            role: this.buildWebRole(metaData),
+            instanceType: EC2.InstanceType.of(EC2.InstanceClass.BURSTABLE3, EC2.InstanceSize.MICRO),
+            associatePublicIpAddress: false,
+            desiredCapacity: 2,
+            maxCapacity: 4,
+            minCapacity:0,
+            autoScalingGroupName: asgName,
+            userData: userData
+        });
+
+        Core.Tags.of(asg).add(metaData.NAME, metaData.PREFIX+"web-asg");
+        Core.Tags.of(asg).add(metaData.NAME, metaData.PREFIX+"web-ec2", { includeResourceTypes: [EC2.CfnInstance.CFN_RESOURCE_TYPE_NAME] });
+        return asg;
+    }
     
     private buildWebSecurityGroup(metaData:MetaData, loadBalancerSecurityGroup:EC2.ISecurityGroup): EC2.ISecurityGroup {
         var securityGroup = new EC2.SecurityGroup(this,metaData.PREFIX+"web-sg", {
@@ -109,10 +126,6 @@ export class ClassicWebStackL2 extends Core.Stack {
         });
         Core.Tags.of(webRole).add(metaData.NAME, metaData.PREFIX+"web-role");
         return webRole;
-    }
-    
-    private createLoadBalancer(metaData: MetaData) {
-        
     }
     
     // https://docs.aws.amazon.com/codedeploy/latest/userguide/codedeploy-agent-operations-install-linux.html
