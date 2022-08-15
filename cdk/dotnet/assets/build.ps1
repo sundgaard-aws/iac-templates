3dbc81cdb813eda0de3d34208024dcad445e61aa
Michael Sundgaard <sundgaar@amazon.com>
2022-08-15 09:20:00 +0200

    Added build script

---
+++ cdk/dotnet/assets/build.ps1
@@ -0,0 +1,14 @@
+Write-Output "Building Submit Function"
+cd SubmitFunctionHandler
+dotnet publish -o publish
+cd publish
+#7z a -sdel ../bin/drop.zip *
+7z a ../bin/drop.zip *
+
+cd ../..
+
+Write-Output "Building JobStatus Function"
+cd JobStatusFunctionHandler
+dotnet publish -o publish
+cd publish
+7z a ../bin/drop.zip *
